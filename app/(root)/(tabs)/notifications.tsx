ebb57f32be25ebd160280e1f5d5fcf1319469cea
OMI786I <sohrawardy1998@gmail.com>
2025-02-16 13:29:23 +0600

    designed notification page

--- app/(root)/(tabs)/notifications.tsx
+++ app/(root)/(tabs)/notifications.tsx
@@ -4,6 +4,7 @@ import {
   SafeAreaView,
   ScrollView,
   TouchableOpacity,
+  Image,
 } from "react-native";
 import React from "react";
 import AntDesign from "@expo/vector-icons/AntDesign";
@@ -11,7 +12,21 @@ import AntDesign from "@expo/vector-icons/AntDesign";
 const Notifications = () => {
   return (
     <SafeAreaView className="bg-[#f9f9f9] min-h-screen p-5">
-      <ScrollView></ScrollView>
+      <ScrollView>
+        <View className="bg-white justify-center pb-40 items-center min-h-full ">
+          <Image
+            className="mb-6"
+            source={require("@/assets/images/bell.png")}
+          ></Image>
+          <Text className="font-bold text-3xl ">No Notifications!</Text>
+          <Text className="text-[#b0b0b0] mt-6">
+            You don't have any notification yet. Please place order
+          </Text>
+          <TouchableOpacity className="bg-[#6759ff] p-5 rounded-xl mt-14">
+            <Text className="text-white">View all Services</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
