82191dabcc106a1cca1c48b2b18aa461a97224ce
OMI786I <sohrawardy1998@gmail.com>
2025-02-20 23:11:56 +0600

    added top image

--- app/(root)/pages/[id].tsx
+++ app/(root)/pages/[id].tsx
@@ -1,9 +1,18 @@
-import { View, Text, ScrollView, Image, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import React, { useEffect, useState } from "react";
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { data, Data } from "@/constants/data";
-
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { goBack } from "expo-router/build/global-state/routing";
 const DetailsPage: React.FC = () => {
+  const router = useRouter();
   const { id } = useLocalSearchParams();
   const [detailsData, setDetailsData] = useState<Data | null>(null);
 
@@ -20,12 +29,30 @@ const DetailsPage: React.FC = () => {
     return (
       <SafeAreaView>
         <ScrollView>
-          <View>
+          {/**top image */}
+          <View className="relative">
             <Image
               source={detailsData.image}
-              className="w-[100%] h-80"
+              className="w-[100%] h-96"
               resizeMode="cover"
             />
+            <View className="absolute px-6 py-10">
+              <TouchableOpacity className="mb-14" onPress={() => router.back()}>
+                <AntDesign name="arrowleft" size={30} />
+              </TouchableOpacity>
+
+              {/**rating */}
+              <View className="bg-[#FB9450] p-2 z-10 flex-row items-center gap-1 w-[23%] rounded-xl">
+                <AntDesign name="star" size={12} color="white" />
+                <Text className="text-white">{detailsData.rating}</Text>
+              </View>
+              {/**title */}
+              <View className="w-[100%] mt-6">
+                <Text className="font-bold text-5xl text-white">
+                  AC Regular Service
+                </Text>
+              </View>
+            </View>
           </View>
         </ScrollView>
       </SafeAreaView>
